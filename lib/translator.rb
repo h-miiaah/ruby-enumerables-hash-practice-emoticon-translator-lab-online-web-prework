e8c4d2abe42f98529fe1f9bbd87d2152eba95971
Haim D <haim11293@aol.com>
2019-07-26 23:45:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,12 @@ def get_japanese_emoticon(path, emoticon)
   result
 end
 
-def get_english_meaning
+def get_english_meaning(path, emoticon)
   # code goes here
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_meaning"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found"
+  end
+  result
 end
